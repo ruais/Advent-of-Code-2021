6eb452fe596e5ec634d062bd6a37aef372beb083
ruais <51882764+ruais@users.noreply.github.com>
2021-12-10 18:49:50 +1100

    Update day10.py
    merge everything into one function

--- AOC2021/day10.py
+++ AOC2021/day10.py
@@ -3,40 +3,10 @@
 with open(r'.\input\day10.txt') as file:
     data = file.read().split('\n')
 
-def syntax_corruption_score(data):
-    pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
-    score = {')': 3, ']': 57, '}': 1197, '>': 25137}
-    def opengroup():
-        match = line.pop(0)
-        while True:
-            if not line:
-                return None
-            elif line[0] in pairs.values():
-                if line[0] == pairs[match]:
-                    line.pop(0)
-                    return True
-                else:
-                    raise SyntaxError(line[0])
-            else:
-                inner = opengroup()
-                if not inner:
-                    return inner
-
-    total = 0
-    for line in data:
-        line = list(line)
-        while line:
-            try:
-                opengroup()
-            except SyntaxError as err:
-                total += score[str(err).replace('SyntaxError: ', '')]
-                break
-
-    return total
-
 def syntax_error_correction(data):
     pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
-    score = {')': 1, ']': 2, '}': 3, '>': 4}
+    correction_score = {')': 1, ']': 2, '}': 3, '>': 4}
+    corruption_score = {')': 3, ']': 57, '}': 1197, '>': 25137}
     def opengroup(correction):
         match = line.pop(0)
         while True:
@@ -52,6 +22,7 @@ def syntax_error_correction(data):
             else:
                 correction = opengroup(correction)
 
+    line_corruptions = []
     line_corrections = []
     for line in data:
         line = list(line)
@@ -60,18 +31,20 @@ def syntax_error_correction(data):
                 correction = opengroup('')
                 total = 0
                 for char in correction:
-                    total = total * 5 + score[char]
+                    total = total * 5 + correction_score[char]
                 line_corrections.append((correction, total))
-            except SyntaxError:
+            except SyntaxError as err:
+                linescore = corruption_score[str(err)[-1]]
+                line_corruptions.append(linescore)
                 break
 
-    return tuple(line_corrections)
+    return tuple(line_corrections), sum(line_corruptions)
 
 def solveA():
-    print(syntax_corruption_score(data))
+    print(syntax_error_correction(data)[1])
 
 def solveB():
-    corrections = list(syntax_error_correction(data))
+    corrections = list(syntax_error_correction(data)[0])
     corrections.sort(key=lambda x: x[1])
     median = int((len(corrections)-1)/2)
     print(corrections[median][1])
