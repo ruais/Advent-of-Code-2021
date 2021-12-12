aaabffd89b3d762fdc6f774463fb102d0d8cdc79
ruais <51882764+ruais@users.noreply.github.com>
2021-12-13 10:35:33 +1100

    Update day11.py
    reduced the amount of times the Neighbour object is generated to one

--- AOC2021/day11.py
+++ AOC2021/day11.py
@@ -1,6 +1,66 @@
 # advent of code day 11 by Ruma (Lynn)
 
 class Plot:
+    class Neighbour:
+        def __init__(self, arg, plot):
+            keywords = ('t', 'x', 'o', 's')
+
+            surr = set()
+            selfinclus = False
+            minx = miny = maxx = maxy = 0
+
+            arg = arg[2:].split('|')
+            for part in arg:
+                part = [coord.split(',') for coord in part.split(':')]
+                assert len(part) < 3
+                for j in range(len(part)):
+                    if part[j][0] == 'self':
+                        part[j] = [0, 0]
+                        continue
+                    num = part[j][0].strip(' ').strip('-').isnumeric()
+                    part[j] = [int(a) if num else a.strip(' ') for a in part[j]]
+
+                if part[0][0] in keywords:
+                    assert all(len(p) == 1 for p in part)
+                    kw = part[0][0]
+                    reg = part[-1][0]
+                    if minx > -reg: minx = -reg
+                    if miny > -reg: miny = -reg
+                    if maxx <= reg: maxx = reg + 1
+                    if maxy <= reg: maxy = reg + 1
+
+                    if kw == 't':
+                        surr.update(set(plot.__lin__((-reg,0), (reg,0))))
+                        surr.update(set(plot.__lin__((0,-reg), (0,reg))))
+                    if kw == 'x':
+                        surr.update(set(plot.__lin__((-reg,-reg), ( reg, reg))))
+                        surr.update(set(plot.__lin__((-reg, reg), ( reg,-reg))))
+                    if kw == 'o':
+                        surr.update(set(plot.__lin__((-reg,-reg), ( reg,-reg))))
+                        surr.update(set(plot.__lin__((-reg, reg), ( reg, reg))))
+                        surr.update(set(plot.__lin__((-reg,-reg), (-reg, reg))))
+                        surr.update(set(plot.__lin__(( reg,-reg), ( reg, reg))))
+                    if kw == 's':
+                        surr.update(set(plot.__sqr__((-reg,-reg), (reg, reg))))
+                else:
+                    range0, range1 = plot.__range__(part[0], part[-1])
+                    if minx > range0[0]: minx = range0[0]
+                    if miny > range0[1]: miny = range0[1]
+                    if maxx <= range1[0]: maxx = range1[0]
+                    if maxy <= range1[1]: maxy = range1[1]
+                    if any(coord == [0, 0] for coord in part):
+                        selfinclus = True
+                    surr.update(set(plot.__lin__(part[0], part[-1])))
+
+            if not selfinclus:
+                surr -= {(0,0)}
+
+            self.coords = surr
+            self.range = ((minx, miny), (maxx, maxy))
+
+        def __getitem__(self, index):
+            return (self.coords, *self.range)[index]
+
     def __init__(self, init0 = (0, 0), init1 = ((0,),)):
         self.x, self.y = init0
         self.upper = lambda: (self.x, self.y)
@@ -97,64 +157,17 @@ class Plot:
             func = lambda _, x: x
             args = [f]
 
-        return func, tuple(args)
+        for i in range(len(args)):
+            arg = args[i]
+            if isinstance(arg, str) and arg.strip('\\').startswith('--'):
+                if arg.startswith('\\'):
+                    args[i] = arg[1:]
+                else:
+                    args[i] = self.Neighbour(arg, self)
+
+        return (func,) + tuple(args)
 
     def pull(self, coords, data = True, args = None):
-        def parsearg(arg):
-            keywords = ('t', 'x', 'o', 's')
-
-            surr = set()
-            selfinclus = False
-            minx = miny = maxx = maxy = 0
-
-            arg = arg[2:].split('|')
-            for part in arg:
-                part = [coord.split(',') for coord in part.split(':')]
-                assert len(part) < 3
-                for j in range(len(part)):
-                    if part[j][0] == 'self':
-                        part[j] = [0, 0]
-                        continue
-                    num = part[j][0].strip(' ').strip('-').isnumeric()
-                    part[j] = [int(a) if num else a.strip(' ') for a in part[j]]
-
-                if part[0][0] in keywords:
-                    assert all(len(p) == 1 for p in part)
-                    kw = part[0][0]
-                    reg = part[-1][0]
-                    if minx > -reg: minx = -reg
-                    if miny > -reg: miny = -reg
-                    if maxx <= reg: maxx = reg + 1
-                    if maxy <= reg: maxy = reg + 1
-
-                    if kw == 't':
-                        surr.update(set(self.__lin__((-reg,0), (reg,0))))
-                        surr.update(set(self.__lin__((0,-reg), (0,reg))))
-                    if kw == 'x':
-                        surr.update(set(self.__lin__((-reg,-reg), ( reg, reg))))
-                        surr.update(set(self.__lin__((-reg, reg), ( reg,-reg))))
-                    if kw == 'o':
-                        surr.update(set(self.__lin__((-reg,-reg), ( reg,-reg))))
-                        surr.update(set(self.__lin__((-reg, reg), ( reg, reg))))
-                        surr.update(set(self.__lin__((-reg,-reg), (-reg, reg))))
-                        surr.update(set(self.__lin__(( reg,-reg), ( reg, reg))))
-                    if kw == 's':
-                        surr.update(set(self.__sqr__((-reg,-reg), (reg, reg))))
-                else:
-                    range0, range1 = self.__range__(part[0], part[1])
-                    if minx > range0[0]: minx = range0[0]
-                    if miny > range0[1]: miny = range0[1]
-                    if maxx <= range1[0]: maxx = range1[0]
-                    if maxy <= range1[1]: maxy = range1[1]
-                    if any(coord == [0, 0] for coord in part):
-                        selfinclus = True
-                    surr.update(set(self.__lin__(part[0], part[-1])))
-
-            if not selfinclus:
-                surr -= {(0,0)}
-
-            return surr, (minx, miny), (maxx, maxy)
-
         i, j = self.pos(coords)
         if None not in (i, j):
             obj = self.array[j][i]
@@ -165,21 +178,16 @@ class Plot:
         if args is not None: args = list(args)
         for i in range(len(args or ())):
             arg = args[i]
-            if isinstance(arg, str) and arg.strip('\\').startswith('--'):
-                if arg.startswith('\\'):
-                    args[i] = arg[1:]
-                else:
-                    surrounds, range0, range1 = parsearg(arg)
+            if isinstance(arg, Plot.Neighbour):
+                surrounds, range0, range1 = tuple(arg)
+                arg = []
+                for y in range(range0[1], range1[1]):
+                    for x in range(range0[0], range1[0]):
+                        if (x,y) in surrounds:
+                            s = self.pull((coords[0]+x, coords[1]+y), data)
+                            arg.append(s)
 
-                    x0, y0, x1, y1 = range0 + range1
-                    arg = []
-                    for y in range(y0, y1 + 1):
-                        for x in range(x0, x1 + 1):
-                            if (x,y) in surrounds:
-                                s = self.pull((coords[0]+x, coords[1]+y), data)
-                                arg.append(s)
-
-                    args[i] = tuple(arg) if len(arg) > 1 else arg[0]
+                args[i] = tuple(arg) if len(arg) > 1 else arg[0]
 
         if args is not None:
             if len(args) == 1 and isinstance(args[0], tuple):
@@ -190,7 +198,7 @@ class Plot:
 
     def mutate(self, alter, *coords):
         self.expand(*coords)
-        alter, args = self.parsefunc(alter)
+        alter, *args = self.parsefunc(alter)
         for c in coords:
             cargs = self.pull(c, args = args)
             self[c] = alter(*cargs)
@@ -213,8 +221,8 @@ class Plot:
         x[1] += 1
         y[1] += 1
 
-        filt, fargs = self.parsefunc(filt or True)
-        alter, aargs = self.parsefunc(alter or self.find)
+        filt, *fargs = self.parsefunc(filt or True)
+        alter, *aargs = self.parsefunc(alter or self.find)
 
         finds = []
         for y2 in range(*y):
@@ -230,11 +238,9 @@ def plotmap(data):
     data = tuple(tuple(int(n) for n in line) for line in data.split('\n') if line)
     return Plot((0,0), data)
 
-def flash(plot):
+def flash(plot, addneighbours):
     plot.mutatesquare((int.__add__, 1), *plot.range())
-    activecells = list(plot.search(data = False, alter = (
-                lambda c, *neighbour: (c, *(n for n in neighbour if n)), '--o:1'
-                )))
+    activecells = list(plot.search(data = False, alter = addneighbours))
     flashes = 0
     while activecells:
         flashed = 0
@@ -263,10 +269,13 @@ def init():
 def solveA():
     init()
     plot = plotmap(data)
+    addneighbours = plot.parsefunc((
+                lambda c, *neighbour: (c, *(n for n in neighbour if n)), '--o:1'
+                ))
 
     flashes = 0
     for _ in range(100):
-        plot, newflashes = flash(plot)
+        plot, newflashes = flash(plot, addneighbours)
         flashes += newflashes
 
     return flashes
@@ -274,12 +283,14 @@ def solveA():
 def solveB():
     init()
     plot = plotmap(data)
+    addneighbours = plot.parsefunc((
+                lambda c, *neighbour: (c, *(n for n in neighbour if n)), '--o:1'
+                ))
 
     steps = 0
-    while True:
+    flashes = 0
+    while flashes < plot.width() * plot.height():
         steps += 1
-        plot, flashes = flash(plot)
-        if flashes == plot.width() * plot.height():
-            break
+        plot, flashes = flash(plot, addneighbours)
 
     return steps
